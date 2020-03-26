485546cbe886bd7c86d48446e378dd9aaa471a6e
Dimonyga <Dimonyga@gmail.com>
2020-03-26 17:10:36 +0300

    fix FPU

--- HAL/STM32/system_stm32f4xx.c
+++ HAL/STM32/system_stm32f4xx.c
@@ -215,7 +215,7 @@ static void SetSysClock(void);
 void SystemInit(void)
 {
   /* FPU settings ------------------------------------------------------------*/
-  #if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
+  #if (__FPU_PRESENT == 1)
     SCB->CPACR |= ((3UL << 10*2)|(3UL << 11*2));  /* set CP10 and CP11 Full Access */
   #endif
   /* Reset the RCC clock configuration to the default reset state ------------*/
